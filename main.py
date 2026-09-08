3fa48cdfbfaaa79c8fa3c5c812303762a32511a6
ddroll <sashagast02@gmail.com>
2026-09-09 01:14:20 +0200

    Webserver +

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 from dotenv import load_dotenv
 import logging
+from aiohttp import web
 
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand, BotCommandScopeChat, BotCommandScopeDefault
@@ -9,6 +10,7 @@ from handlers import router
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+PORT = os.getenv("PORT", 10000)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -21,6 +23,28 @@ bot = Bot(TOKEN)
 dp = Dispatcher()
 dp.include_router(router)
 
+async def nudge(_: web.Request):
+    return web.Response(text='OK', status=200)
+
+async def boot_webserver():
+    try:
+        app = web.Application()
+
+        app.router.add_get('/', nudge)
+
+        runner = web.AppRunner(app)
+        await runner.setup()
+
+        site = web.TCPSite(runner, '0.0.0.0', PORT)
+        await site.start()
+
+        logger.info('Webserver started successfully')
+        return runner
+
+    except Exception as e:
+        logger.error(f'Error while starting webserver: {e}')
+
+
 async def setup_commands(bot: Bot):
     commands = [
         BotCommand(command="info", description="info hub")
@@ -31,10 +55,22 @@ async def setup_commands(bot: Bot):
     )
 
 async def main():
-    dp.startup.register(setup_commands)
+    runner = None
+    try:
+        runner = await boot_webserver()
+    except Exception as e:
+        logger.error(f'Error while starting webserver: {e}')
 
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    try:
+        dp.startup.register(setup_commands)
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot)
+    except Exception as e:
+        logger.error(f'Error while starting Bot polling: {e}')
+    finally:
+        if runner:
+            await runner.cleanup()
+            logger.info('Webserver was shut down successfully')
 
 if __name__ == "__main__":
     asyncio.run(main())
